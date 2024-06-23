373f536be29080c50d8bceaabad3b17a6489ea1c
“Lucas <lucasdre4@gmail.com>
2024-06-23 15:45:10 -0300

    :tada: initial commit

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pego a tag da imagem
+  const img = document.querySelector("#profile img")
+  // substituo a imagem
+  if (html.classList.contains("light")) {
+    //se tiver em light mode, adiciono a imagem light
+    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute(
+      "alt",
+      "Lucas alexandre usando vestimenta social tirando uma foto em frente ao banheiro"
+    )
+    //se tiver sem o light mode, mantenho a imagem normal
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Lucas Alexandre de roupa social em frente ao espelho do banheiro"
+    )
+  }
+}
