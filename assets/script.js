c4b8e51a250ef9b19b1267a5c735090ffaee747a
“Lucas <lucasdre4@gmail.com>
2024-06-23 18:19:15 -0300

    Alterações de profile perfil

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,14 @@ function toggleMode() {
   // substituo a imzagem
   if (html.classList.contains("light")) {
     //se tiver em light mode, adiciono a imagem light
-    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
       "Lucas alexandre usando vestimenta social tirando uma foto em frente ao banheiro"
     )
     //se tiver sem o light mode, mantenho a imagem normal
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Lucas Alexandre de roupa social em frente ao espelho do banheiro"
-    )
+    img.setAttribute("src", "./assets/batman.png")
+    img.setAttribute("alt", "Foto do batman")
   }
 }
